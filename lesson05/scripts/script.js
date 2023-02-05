30f5abfe4831173d944d61cefc2a78d9d51982e4
ejctchn <ej.ctchn1@gmail.com>
2023-02-04 23:25:52 -0700

    adding bom - top 10

---
+++ lesson05/scripts/script.js
@@ -0,0 +1,26 @@
+const btn = document.querySelector('#submit');
+const list = document.querySelector('#list');
+const input = document.querySelector('#favchap');
+
+btn.addEventListener("click", function() 
+{ 
+    if(input.value.length = 0)
+    {
+        alert("There was nothing entered in text box. Please enter a book and chapter");
+    }
+    else
+    {
+        let li = document.createElement("li");
+        let deleteB = document.createElement('button');
+        li.innerText = input.value;
+        deleteB.textContent = '❌';
+        li.appendChild(deleteB);
+        list.appendChild(li);
+
+        deleteB.addEventListener("click", function()
+        {
+            list.removeChild(li);
+        })
+    }
+})
+
