61cf29ce3a176e4fc41d93bb3090f14d77393cc1
Eli Cutchen <65992004+ejctchn@users.noreply.github.com>
2023-02-22 01:20:23 -0700

    Corrections to Visit counter on Discover

--- scripts/visits.js
+++ scripts/visits.js
@@ -1,15 +1,17 @@
 let visits = localStorage.getItem('visits');
+let vis = 0;
 let message = "";
 
-if(visits = null)
+if(visits == null)
 {
     message = "Welcome to Table Top Chamber! This is your first visit!"
     localStorage.setItem('visits', 1);
 }
 else
 {
-    visits += 1;
-    message = "Welcome back to Table Top Chamber! You've visited " + visits + " times!"
-    localStorage.setItem('visits', visits);
+    vis = parseInt(visits); 
+    vis += 1;
+    message = "<div id='message'>Welcome back to Table Top Chamber! You've visited " + vis + " times!</div>";
+    localStorage.setItem('visits', vis);
 }
 document.querySelector('#message').outerHTML = message;
